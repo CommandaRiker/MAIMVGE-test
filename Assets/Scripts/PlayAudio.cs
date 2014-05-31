ee0186d5cfa27a0cf975edf6887aeefc66fd0c3b
Henrike Lode <Machineers@guest-154-45.bth.se>
2014-05-31 18:12:21 +0200

    movie texture trial

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudio : MonoBehaviour {
+	 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovieTexture clip1 = (MovieTexture)renderer.material.mainTexture;
+		if (Input.GetButtonDown ("Jump")) {
+
+		
+			if (clip1.isPlaying) {
+				clip1.Pause();
+			}
+			else {
+				clip1.Play();
+			}
+		}
+
+		//renderer.material.mainTexture.Play();
+
+	}
+}
